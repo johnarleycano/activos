5f299643e97155a8e0d65792e5de5504670303f3
John Arley <johnarleycano@hotmail.com>
2017-12-15 09:04:49 -0500

    Primer commit

---
+++ application/views/core/header.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Título que viene desde el controlador de cada interfaz -->
+<title><?php echo $titulo; ?></title>
+
+<!-- Estilos -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.min.css" /><!-- Estilos para UI Kit -->
+
+<!-- Scripts -->
+<script src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script> <!-- jQuery -->
+<script src="<?php echo base_url(); ?>js/uikit.min.js"></script> <!-- Scripts para UI Kit -->
+<script src="<?php echo base_url(); ?>js/uikit-icons.min.js"></script> <!-- Scripts para UI Kit -->
+<script src="<?php echo base_url(); ?>js/funciones.js"></script> <!-- Funciones generales -->
