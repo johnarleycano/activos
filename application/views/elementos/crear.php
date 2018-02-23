1e5a381aa4327cf4fbf3e3554e2ee9aa6330ffb0
John Arley <johnarleycano@hotmail.com>
2018-02-23 09:36:47 -0500

    [0.04.01] style(elementos): Cambio en la interfaz de creación
    Modificado formulario para dividirlo en dos columnas

--- application/views/elementos/crear.php
+++ application/views/elementos/crear.php
@@ -1,129 +1,131 @@
-<form class="uk-form-horizontal uk-margin-large">
-	<h4 class="uk-heading-line uk-text-right"><span>Características</span></h4>
-	<div class="uk-margin">
-        <label class="uk-form-label" for="select_tipo_activo">Tipo de activo *</label>
-		<div class="uk-form-controls">
-            <select class="uk-select" id="select_tipo_activo" title="Tipo de activo" autofocus>
-            	<option value="">Seleccione...</option>}
-            	<?php foreach($this->configuracion_model->obtener("tipos_activos") as $tipo_activo){ ?>
-	                <option value="<?php echo $tipo_activo->Pk_Id; ?>"><?php echo $tipo_activo->Nombre; ?></option>
-                <?php } ?>
-            </select>
-        </div>
+<form>
+	<div class="uk-column-1-2@m uk-column-divider">
+		<h4 class="uk-heading-line uk-text-right"><span>Características</span></h4>
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="select_tipo_activo">Tipo de activo *</label>
+			<div class="uk-form-controls">
+	            <select class="uk-select" id="select_tipo_activo" title="Tipo de activo" autofocus>
+	            	<option value="">Seleccione...</option>}
+	            	<?php foreach($this->configuracion_model->obtener("tipos_activos") as $tipo_activo){ ?>
+		                <option value="<?php echo $tipo_activo->Pk_Id; ?>"><?php echo $tipo_activo->Nombre; ?></option>
+	                <?php } ?>
+	            </select>
+	        </div>
+		</div>
+
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="select_clasificacion">Clasificación *</label>
+			<div class="uk-form-controls">
+	            <select class="uk-select" id="select_clasificacion" title="Clasificación">
+	            	<option value="">Seleccione primero un tipo de activo</option>}
+	            </select>
+	        </div>
+		</div>
+
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="select_marca">Marca *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_marca" title="Marca">
+	            	<option value="">Seleccione...</option>
+	            	<?php foreach($this->configuracion_model->obtener("marcas") as $marca){ ?>
+		                <option value="<?php echo $marca->Pk_Id; ?>"><?php echo $marca->Nombre; ?></option>
+	                <?php } ?>
+	            </select>
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="select_modelo">Modelo *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_modelo" title="Modelo">
+	            	<option value="">Elija primero una marca...</option>
+	            </select>
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="select_color">Color *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_color" title="Color">
+	            	<option value="">Seleccione...</option>
+	            	<?php foreach($this->configuracion_model->obtener("colores") as $color){ ?>
+		                <option value="<?php echo $color->Pk_Id; ?>"><?php echo $color->Nombre; ?></option>
+	                <?php } ?>
+	            </select>
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="select_estado">Estado *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_estado" title="Estado">
+	            	<option value="">Seleccione...</option>
+	            	<?php foreach($this->configuracion_model->obtener("estados_elementos") as $estado){ ?>
+		                <option value="<?php echo $estado->Pk_Id; ?>"><?php echo $estado->Nombre; ?></option>
+	                <?php } ?>
+	            </select>
+	        </div>
+	    </div>
+
+		<h4 class="uk-heading-line uk-text-right"><span>Ubicación</span></h4>
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="select_oficina">Oficina *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_oficina" title="Oficina">
+	            	<option value="">Seleccione...</option>}
+	            	<?php foreach($this->configuracion_model->obtener("oficinas") as $oficina){ ?>
+		                <option value="<?php echo $oficina->Pk_Id; ?>"><?php echo $oficina->Nombre; ?></option>
+	                <?php } ?>
+	            </select>
+	        </div>
+	    </div>
+	    
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="select_bloque">Bloque *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_bloque" title="Bloque">
+	            	<option value="">Elija primero una oficina...</option>
+	            </select>
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="select_area">Área *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_area" title="Área">
+	            	<option value="">Elija primero un bloque...</option>
+	            </select>
+	        </div>
+	    </div>
+		
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="select_proveedor">Proveedor *</label>
+	        <div class="uk-form-controls">
+	            <select class="uk-select" id="select_proveedor" title="Proveedor">
+	            	<option value="">Seleccione...</option>}
+	            	<?php foreach($this->configuracion_model->obtener("proveedores") as $proveedor){ ?>
+		                <option value="<?php echo $proveedor->Pk_Id; ?>"><?php echo $proveedor->Nombre; ?></option>
+	                <?php } ?>
+	            </select>
+	        </div>
+	    </div>
+
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="input_fecha_compra">Fecha de compra *</label>
+	        <div class="uk-form-controls">
+	            <input class="uk-input" type="date" id="input_fecha_compra" title="Fecha de compra">
+	        </div>
+	    </div>
+
+		<div class="uk-margin">
+	        <label class="uk-form-label" for="input_valor">Valor</label>
+	        <div class="uk-form-controls">
+	            <input class="uk-input" type="number" min="0" id="input_valor" title="Valor">
+	        </div>
+	    </div>
 	</div>
 
-	<div class="uk-margin">
-        <label class="uk-form-label" for="select_clasificacion">Clasificación *</label>
-		<div class="uk-form-controls">
-            <select class="uk-select" id="select_clasificacion" title="Clasificación">
-            	<option value="">Seleccione primero un tipo de activo</option>}
-            </select>
-        </div>
-	</div>
-
-	<div class="uk-margin">
-        <label class="uk-form-label" for="select_marca">Marca *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_marca" title="Marca">
-            	<option value="">Seleccione...</option>
-            	<?php foreach($this->configuracion_model->obtener("marcas") as $marca){ ?>
-	                <option value="<?php echo $marca->Pk_Id; ?>"><?php echo $marca->Nombre; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <label class="uk-form-label" for="select_modelo">Modelo *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_modelo" title="Modelo">
-            	<option value="">Elija primero una marca...</option>
-            </select>
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <label class="uk-form-label" for="select_color">Color *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_color" title="Color">
-            	<option value="">Seleccione...</option>
-            	<?php foreach($this->configuracion_model->obtener("colores") as $color){ ?>
-	                <option value="<?php echo $color->Pk_Id; ?>"><?php echo $color->Nombre; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <label class="uk-form-label" for="select_estado">Estado *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_estado" title="Estado">
-            	<option value="">Seleccione...</option>
-            	<?php foreach($this->configuracion_model->obtener("estados_elementos") as $estado){ ?>
-	                <option value="<?php echo $estado->Pk_Id; ?>"><?php echo $estado->Nombre; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-    </div>
-
-	<h4 class="uk-heading-line uk-text-right"><span>Ubicación</span></h4>
-	<div class="uk-margin">
-        <label class="uk-form-label" for="select_oficina">Oficina *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_oficina" title="Oficina">
-            	<option value="">Seleccione...</option>}
-            	<?php foreach($this->configuracion_model->obtener("oficinas") as $oficina){ ?>
-	                <option value="<?php echo $oficina->Pk_Id; ?>"><?php echo $oficina->Nombre; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-    </div>
-    
-    <div class="uk-margin">
-        <label class="uk-form-label" for="select_bloque">Bloque *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_bloque" title="Bloque">
-            	<option value="">Elija primero una oficina...</option>
-            </select>
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <label class="uk-form-label" for="select_area">Área *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_area" title="Área">
-            	<option value="">Elija primero un bloque...</option>
-            </select>
-        </div>
-    </div>
-	
-	<div class="uk-margin">
-        <label class="uk-form-label" for="select_proveedor">Proveedor *</label>
-        <div class="uk-form-controls">
-            <select class="uk-select" id="select_proveedor" title="Proveedor">
-            	<option value="">Seleccione...</option>}
-            	<?php foreach($this->configuracion_model->obtener("proveedores") as $proveedor){ ?>
-	                <option value="<?php echo $proveedor->Pk_Id; ?>"><?php echo $proveedor->Nombre; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-    </div>
-
-	<div class="uk-margin">
-        <label class="uk-form-label" for="input_fecha_compra">Fecha de compra *</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" type="date" id="input_fecha_compra" title="Fecha de compra">
-        </div>
-    </div>
-
-	<div class="uk-margin">
-        <label class="uk-form-label" for="input_valor">Valor</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" type="number" min="0" id="input_valor" title="Valor">
-        </div>
-    </div>
-
-    <button class="uk-button uk-button-default uk-modal-close" type="button">Cancelar</button>
+	<button class="uk-button uk-button-default uk-modal-close" type="button">Cancelar</button>
     <button class="uk-button uk-button-primary" type="submit">Guardar</button>
 </form>
 
