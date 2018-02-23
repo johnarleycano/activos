780a2340a4666c07f5a2c7f3fa7c4103f41dcf77
John Arley <johnarleycano@hotmail.com>
2018-02-23 08:53:43 -0500

    [0.03.04] refactor(header): Configurada carga de librerías para entornos
    Se carga las librerías de acuerdo a cada uno de los entornos de desarrollo

--- application/views/core/header.php
+++ application/views/core/header.php
@@ -2,14 +2,31 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- Título que viene desde el controlador de cada interfaz -->
-<title><?php echo $titulo; ?></title>
+<title><?php echo $titulo; ?> | Activos Fijos</title>
+
+<!-- Scripts -->
+<script src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
+
+<?php if(ENVIRONMENT === 'development') { ?>
+	<!-- Estilos -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.css" />
+
+	<!-- Scripts -->
+	<script src="<?php echo base_url(); ?>js/uikit.js"></script>
+	<script src="<?php echo base_url(); ?>js/uikit-icons.js"></script>
+<?php } ?>
+
+<?php if(ENVIRONMENT === 'production') { ?>
+	<!-- Estilos -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.min.css" />
+
+	<!-- Scripts -->
+	<script src="<?php echo base_url(); ?>js/uikit.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/uikit-icons.min.js"></script>
+<?php } ?>
 
 <!-- Estilos -->
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.min.css" /><!-- Estilos para UI Kit -->
 <link rel="stylesheet" href="<?php echo base_url(); ?>css/estilos.css" /><!-- Estilos generales -->
 
 <!-- Scripts -->
-<script src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script> <!-- jQuery -->
-<script src="<?php echo base_url(); ?>js/uikit.min.js"></script> <!-- Scripts para UI Kit -->
-<script src="<?php echo base_url(); ?>js/uikit-icons.min.js"></script> <!-- Scripts para UI Kit -->
 <script src="<?php echo base_url(); ?>js/funciones.js"></script> <!-- Funciones generales -->
